98e4ad9a414f6f7ea1ad679edcb5a790688ee579
Barri <barrifaryad@gmail.com>
2022-04-12 12:32:14 +0100

    pop ups and tooltips and stub

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -249,7 +249,7 @@ describe("My first suite", () => {
     });
   });
 
-  it.only("datepicker test-- assert property", () => {
+  it("datepicker test-- assert property", () => {
 
     function selectDayFormCurrent(day) {
 
@@ -287,4 +287,40 @@ describe("My first suite", () => {
         cy.wrap(input).invoke("prop", "value").should("contain", dateAssert);
       });
   });
+
+   it("tooltip", () => {
+       cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Tooltip").click();
+
+    cy.contains("nb-card", "Colored Tooltips").contains("Default").click();
+    cy.get('nb-tooltip').should('contain','This is a tooltip');
+   })
+
+   it.only("dialog box", () => {
+       cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    //1
+    cy.get('tbody tr').first().find('.nb-trash').click()
+    cy.on('window:confirm' , (confirm) => {
+      expect(confirm).to.equal('Are you sure you want to delete?')
+    })
+
+    // 2 stub method
+    const stub = cy.stub();
+    cy.on('window:confirm', stub)
+    cy.get("tbody tr").first().find(".nb-trash").click().then( ()=>{
+      expect(stub.getCall(0)).to.be.calledWith(
+        "Are you sure you want to delete?"
+      );
+    })
+    // 3 stub method
+   
+       cy.get("tbody tr").first().find(".nb-trash").click();
+       cy.on("window:confirm", ()=> false) 
+   })
+
+
 });
