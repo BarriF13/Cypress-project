45d9c1bbceb234729437a510c3526eb0e0da5fcb
Barri <barrifaryad@gmail.com>
2022-04-12 11:01:55 +0100

    use date function for setting a date in future

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -135,7 +135,7 @@ describe("My first suite", () => {
           .should("contain", "Mar 30, 2022"); //this test might fail as date must be mocked normally
       });
   });
-  it("checkbox", () => {
+  it("Radio button", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
@@ -155,13 +155,136 @@ describe("My first suite", () => {
       });
   });
 
-  it.only("checkbox", () => {
+  it("checkbox", () => {
     cy.visit("/");
     cy.contains("Modal & Overlays").click();
     cy.contains("Toastr").click();
 
-    cy.get('[type="checkbox"]').eq(0).click({ force: true})
-    cy.get('[type="checkbox"]').eq(0).check({force: true})
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+    cy.get('[type="checkbox"]').eq(0).check({ force: true });
+  });
 
+  it("Lists and dropdown", () => {
+    cy.visit("/");
+    //1
+    // cy.get('nav nb-select').click()
+    // cy.get(".options-list").contains('Dark').click();
+    //  cy.get("nav nb-select").should('contain', 'Dark')
+    // cy.get("nb-layout-header nav ").should('have.css', 'background-color','rgb(34, 43, 69)')
+
+    //2
+    cy.get("nav nb-select").then((dropdown) => {
+      cy.wrap(dropdown).click();
+      cy.get(".options-list nb-option").each((listItem, index) => {
+        const itemText = listItem.text().trim();
+
+        const colors = {
+          Light: "rgb(255, 255, 255)",
+          Dark: "rgb(34, 43, 69)",
+          Cosmic: "rgb(50, 50, 89)",
+          Corporate: "rgb(255, 255, 255)",
+        };
+
+        cy.wrap(listItem).click();
+        cy.wrap(dropdown).should("contain", itemText);
+        cy.get("nb-layout-header nav").should(
+          "have.css",
+          "background-color",
+          colors[itemText]
+        );
+        if (index < 3) {
+          cy.wrap(dropdown).click();
+        }
+      });
+    });
+  });
+  it("table test", () => {
+    cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+    //1
+    cy.get("tbody")
+      .contains("tr", "Larry")
+      .then((tableRow) => {
+        cy.wrap(tableRow).find(".nb-edit").click();
+        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type("25");
+        cy.wrap(tableRow).find(".nb-checkmark").click();
+        cy.wrap(tableRow).find("td").eq(6).should("contain", "25");
+      });
+    //2
+    cy.get("thead").find(".nb-plus").click();
+    cy.get("thead")
+      .find("tr")
+      .eq(2)
+      .then((tableRow) => {
+        cy.wrap(tableRow).find('[placeholder="First Name"]').type("zaghart");
+        cy.wrap(tableRow).find('[placeholder="Last Name"]').type("Hoodhoodi");
+        cy.wrap(tableRow).find(".nb-checkmark").click();
+      });
+    cy.get("tbody tr")
+      .first()
+      .find("td")
+      .then((tableColumn) => {
+        cy.wrap(tableColumn).eq(2).should("contain", "zaghart");
+        cy.wrap(tableColumn).eq(3).should("contain", "Hoodhoodi");
+      });
+    // //3
+    // cy.get('thead [placeholder="Age"]').type('20')
+    // cy.wait(500)
+    // cy.get('tbody tr').each( tableRow =>{
+    //   cy.wrap(tableRow).find('td').eq(6).should('contain' , 20)
+    // })
+    //4
+    const age = [20, 30, 40, 200];
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      cy.get("tbody tr").each((tableRow) => {
+        if (age === 200) {
+          cy.wrap(tableRow).should("contain", "No data found");
+        } else {
+          cy.wrap(tableRow).find("td").eq(6).should("contain", age);
+        }
+      });
+    });
+  });
+
+  it.only("datepicker test-- assert property", () => {
+
+    function selectDayFormCurrent(day) {
+
+      let date = new Date();
+      date.setDate(date.getDate() + day);
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleString("default", { month: "short" });
+      let dateAssert = futureMonth + " " + futureDay + ", " + date.getFullYear();
+
+      cy.get("nb-calendar-navigation")
+        .invoke("attr", "ng-reflect-date")
+        .then((dateAttribute) => {
+          if (!dateAttribute.includes(futureMonth)) {
+            cy.get('[data-name="chevron-right"]').click();
+            selectDayFormCurrent(day);
+          } else {
+            cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]')
+              .contains(futureDay)
+              .click();
+          }
+        });
+        return dateAssert
+    }
+
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Datepicker").click();
+
+    cy.contains("nb-card", "Common Datepicker")
+      .find("input")
+      .then((input) => {
+        cy.wrap(input).click();
+       let dateAssert = selectDayFormCurrent(3);
+
+        cy.wrap(input).invoke("prop", "value").should("contain", dateAssert);
+      });
   });
 });
